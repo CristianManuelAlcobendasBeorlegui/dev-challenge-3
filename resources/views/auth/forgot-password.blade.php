0fad80ba4089074f91863168eca6b099713d1835
Cristian Alcobendas <cristian.alcobendas@insbaixcamp.cat>
2024-10-22 17:30:03 +0200

    Improve login view

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,52 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Forgot your password</title>
+  <meta name="description" content="DevChallenge Login/Register application">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/tabler-io/webfont/tabler-icons.min.css') }}">
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+  <!-- <meta http-equiv="refresh" content="5"> -->
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+  @vite(['resources/js/app.js'])
+</head>
+<body>
+  <section class="section section--forgot-password">
+    <h1 class="forgot-password__heading">Forgot your password?</h1>
+    <p class="forgot-password__description">
+      Don't worry, just let us know your email address and we will send an 
+      email with a password reset link that will allow you to choose a new 
+      one.
+    </p>
+    <form class="form form--forgot-password" action="{{ route('password.email') }}" method="POST">
+      @csrf
+      <div class="form__input-boxes">
+        <div class="form__input-box">
+          <div class="input-box__field">
+            <input type="email" name="email" id="email" class="field__input" required autofocus>
+            <i class="field__icon ti ti-mail-filled"></i>
+          </div>
+          <div class="input-box__status input-box__status--error">
+            @php($messages = $errors->get('email'))
+            @if ($messages)
+              <ul class="error__list-items">
+                @foreach ((array) $messages as $message)
+                  <li class="error__list-item">{{ $message }}</li>
+                @endforeach
+              </ul>
+            @endif
+          </div>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
+      </div>
+      <div class="form__options">
+        <button class="submit-btn">
+          <span>Send password reset link email</span>
+          <i class="submit-btn__icon ti ti-arrow-right"></i>
+        </button>
+      </div>
     </form>
-</x-guest-layout>
+  </section>
+</body>
+</html>
